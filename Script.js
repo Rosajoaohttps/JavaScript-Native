e9feb162658cbd883f802afd8185c2bc22b5066a
João Vitor Da Rosa <joaovitorrosamachado@hotmail.com>
2024-10-23 21:38:54 -0300

    javascript_Config
    inicio do codigo de validação de email e senha

--- Script.js
+++ Script.js
@@ -1,14 +1,33 @@
-//Exemplos de Var
-var Valor = "123456"
-//Exemplo de Let
-let valor2 = "cachorro"
-//Exemplo de Const
-const nome = "José" 
+validar(){
+    const Email = document.getElementById(exampleInputEmail1);
+    const senha = document.getElementById(exampleInputPassword1);
+    const password_user_fixed = ("cachorro123");
+    const email_user_fixed = ("Email_test@gmail.com");
+     
+    if (Email != email_user_fixed) {
+        
+        mensagem.textContent = "Incorreto";
+        Email.value = "";
+    }
+    else if (Email == email_user_fixed){
+        const email_correct = '';
+        Email = email_correct
 
 
+        
+    }
+    if (senha != password_user_fixed) {
+        
+        mensagem.textContent = "Incorreto";
+        senha.value = "";
+    }
+    else if (senha == password_user_fixed){
+        const senha_correct = '';
+        Email = senha_correct
 
-function somatoria(valor){
-    let valor = 1 
-    valor + 1;
 
-}
+        
+    }
+
+   
+};
